f70790a0e66630846456bab9d669974790faf8e2
Yossi <yossi.shelly@gmail.com>
2022-10-07 14:52:56 +0300

    Combined all custom components and library classed into one "Amiga" componeny library.

--- AmigaImageConverter/MainForm.cs
+++ AmigaImageConverter/MainForm.cs
@@ -8,8 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Hexadecimal;
-using AmigaBitplane;
+using Amiga;
 
 
 namespace AmigaImageConverter
