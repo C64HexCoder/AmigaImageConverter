2e833c2af4f348948acd9da1862a956fb6ba39ad
Yossi Shelly <yossi.shelly@gmail.com>
2025-02-16 02:27:55 +0200

    some cleanup to OctreeQuantisation and FreedHGlobal memory.

--- AmigaImageConverter/MainForm.cs
+++ AmigaImageConverter/MainForm.cs
@@ -153,7 +153,7 @@ namespace AmigaImageConverter
                             break;
 
                         case 2:
-                            vr.bitplane.bitmap = OctreeQuantization.ReduceColors(bmp,loadImageDlg.ImageNumOfColors,OctreeQuantization.Algorithem.LessUsed);
+                            vr.bitplane.bitmap = OctreeQuantization.ReduceColors(bmp,loadImageDlg.ImageNumOfColors,OctreeQuantization.Algorithem.CloseColorsAndLessUsed);
                             break;
 
                         case 3:
