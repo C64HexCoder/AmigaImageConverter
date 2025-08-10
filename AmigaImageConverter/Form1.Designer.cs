511130e0039db835f326c20b8629ccf41e1793a8
Yossi Shelly <yossi.shelly@gmail.com>
2025-08-11 01:51:16 +0300

    x

--- AmigaImageConverter/Form1.Designer.cs
+++ AmigaImageConverter/Form1.Designer.cs
@@ -29,62 +29,30 @@ namespace AmigaImageConverter
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            pictureBox2 = new Amiga.PictureBox();
-            hScrollBar1 = new System.Windows.Forms.HScrollBar();
-            vScrollBar1 = new System.Windows.Forms.VScrollBar();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            exPictureBox1 = new Amiga.ExPictureBox();
             SuspendLayout();
             // 
-            // pictureBox2
+            // exPictureBox1
             // 
-            pictureBox2.BackgroundImage = (System.Drawing.Image)resources.GetObject("pictureBox2.BackgroundImage");
-            pictureBox2.HorizontalScrollBar = hScrollBar1;
-            pictureBox2.HScrollBarAlignment = Amiga.PictureBox.HorizontalScrollBarAlignment.Bottom;
-            pictureBox2.Image = null;
-            pictureBox2.InterpolationAlgorithm = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
-            pictureBox2.Location = new System.Drawing.Point(30, 21);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.ScaleFactor = 1F;
-            pictureBox2.ScrollBarsWidth = 30;
-            pictureBox2.Size = new System.Drawing.Size(737, 387);
-            pictureBox2.SizeMode = Amiga.PictureBox.PictureBoxSizeMode.Normal;
-            pictureBox2.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.None;
-            pictureBox2.TabIndex = 1;
-            pictureBox2.TabStop = false;
-            pictureBox2.VerticalScrollBar = vScrollBar1;
-            pictureBox2.VScrollBarAlignment = Amiga.PictureBox.VerticalScrollBarAlignment.Right;
-            // 
-            // hScrollBar1
-            // 
-            hScrollBar1.Location = new System.Drawing.Point(30, 408);
-            hScrollBar1.Name = "hScrollBar1";
-            hScrollBar1.Size = new System.Drawing.Size(737, 30);
-            hScrollBar1.TabIndex = 2;
-            // 
-            // vScrollBar1
-            // 
-            vScrollBar1.Location = new System.Drawing.Point(767, 21);
-            vScrollBar1.Name = "vScrollBar1";
-            vScrollBar1.Size = new System.Drawing.Size(30, 387);
-            vScrollBar1.TabIndex = 3;
+            exPictureBox1.Image = (System.Drawing.Image)resources.GetObject("exPictureBox1.Image");
+            exPictureBox1.Location = new System.Drawing.Point(12, 12);
+            exPictureBox1.Name = "exPictureBox1";
+            exPictureBox1.Size = new System.Drawing.Size(760, 402);
+            exPictureBox1.TabIndex = 4;
             // 
             // Form1
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             ClientSize = new System.Drawing.Size(800, 450);
-            Controls.Add(vScrollBar1);
-            Controls.Add(hScrollBar1);
-            Controls.Add(pictureBox2);
+            Controls.Add(exPictureBox1);
             Name = "Form1";
             Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-        private Amiga.PictureBox pictureBox2;
-        private System.Windows.Forms.HScrollBar hScrollBar1;
-        private System.Windows.Forms.VScrollBar vScrollBar1;
+
+        private Amiga.ExPictureBox exPictureBox1;
     }
 }
